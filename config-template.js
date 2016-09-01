ed2c686646929e930b0ee59c98f303fabfe1d79c
Nathaniel Baer <njbbaer@gmail.com>
2016-09-01 13:59:54 -0700

    Create config-template.js

---
+++ config-template.js
@@ -0,0 +1,16 @@
+/** Must rename file as config.js */
+
+var config = {};
+
+config.buttons = [
+	{
+		name: CUSTOM NAME,
+		mac: MAC ADDRESS,
+		action: ACTION NAME,
+		parameters: [
+			OPTIONAL PARAMETERS
+		]
+	},
+]
+
+module.exports = config;
